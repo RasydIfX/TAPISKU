d8ce3eab610e6d28d63b22ee8ea6d4410021f138
Rasyd <rasyidridhotv@gmail.com>
2026-01-17 23:50:25 +0700

    + lagi

--- web/src/data/products.js
+++ web/src/data/products.js
@@ -2,6 +2,8 @@ import tapisJung from "../assets/Tapis/jung.jpg";
 import tapisAndak from "../assets/Tapis/laut-Andak.jpg";
 import tapisMedal from "../assets/Tapis/raja-medal.jpg";
 import tapisTunggal from "../assets/Tapis/raja-tunggal.jpg";
+import tapisSilung from "../assets/Tapis/tapis silung.jpg";
+import tapisBalak from "../assets/Tapis/tapis-balak.jpg";
 
 export const products = [
   {
@@ -27,5 +29,17 @@ export const products = [
     name: "Tapis Raja Tunggal",
     price: 3200000,
     image: tapisTunggal,
+  },
+  {
+    id: 5,
+    name: "Tapis Silung",
+    price: 3200000,
+    image: tapisSilung,
+  },
+  {
+    id: 6,
+    name: "Tapis Balak",
+    price: 3200000,
+    image: tapisBalak,
   },
 ];
